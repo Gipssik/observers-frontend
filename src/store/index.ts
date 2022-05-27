b578e1f3a0d4e294c357f97b432501e9fa8b3488
Gipss <gissa.dmtr@gmail.com>
2022-05-27 19:18:24 +0300

    Redux devtools fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,10 @@
 import {applyMiddleware, createStore} from "redux";
 import thunk from "redux-thunk";
 import {rootReducer} from "./reducers";
-import {composeWithDevTools} from "redux-devtools-extension";
+// Development
+// import {composeWithDevTools} from "redux-devtools-extension";
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+// Production
+export const store = createStore(rootReducer, applyMiddleware(thunk));
+// Development
+// export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
